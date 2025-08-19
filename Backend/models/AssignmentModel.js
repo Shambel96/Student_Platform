795cb9ade4bab152ed7ec6b4515e52dd496ab34c
Shambel96 <shambeldechu@gmail.com>
2025-08-20 01:14:59 +0300

    feat:new document added at assignment schema

--- Backend/models/AssignmentModel.js
+++ Backend/models/AssignmentModel.js
@@ -1,15 +1,28 @@
 const mongoose = require("mongoose");
 
-const assignmentSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const assignmentSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
   },
-  courseId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Assignment", assignmentSchema);
