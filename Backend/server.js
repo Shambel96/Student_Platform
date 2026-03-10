5cccfabf10a8ef200ea32f275bbcdbff58cb4601
Shambel Dechu <shambeldechu@gmail.com>
2026-03-10 12:15:58 +0300

    chore(app): refcator the server.js file to reduce teh burden

--- Backend/server.js
+++ Backend/server.js
@@ -1,38 +1,10 @@
-// Entry point for the backend server
-const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
-
-// Load env vars
+const app = require("./app");
 dotenv.config();
 
-// Connect to database
-connectDB();
 
-// Route files
-const auth = require("./routers/authRouter");
-const courses = require("./routers/courseRouter");
-const assignments = require("./routers/assignmentRouter");
 
-const app = express();
 const PORT = process.env.PORT || 3333;
-
-// Enable CORS
-app.use(cors());
-
-// Body parser
-app.use(express.json());
-
-// Mount routers
-app.use("/api/auth", auth);
-app.use("/api/courses", courses);
-app.use("/api/assignments", assignments);
-
-// Default route
-app.get("/", (req, res) => {
-  res.end("Welcome to the backend server! Now API is ready to use.");
-});
 
 // Server started here
 app.listen(PORT, () => {
