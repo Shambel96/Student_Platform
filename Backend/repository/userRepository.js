1666b8d9cef10aff4b4d69d4fd8a5a931fbc074c
Shambel Dechu <shambeldechu@gmail.com>
2026-08-04 11:14:39 +0300

    fix: use User model directly instead of deleted controller

--- Backend/repository/userRepository.js
+++ Backend/repository/userRepository.js
@@ -1,8 +1,17 @@
-const database = require("../controllers/StudentControllers");
 const User = require("../models/User");
 
 class UserRepository {
-  async getAllUsers() {
-    return await database.getAllUsers();
+  async findById(id) {
+    return await User.findById(id).select("-password -refreshToken");
+  }
+
+  async findByEmail(email) {
+    return await User.findOne({ email });
+  }
+
+  async create(userData) {
+    return await User.create(userData);
   }
 }
+
+module.exports = new UserRepository();
