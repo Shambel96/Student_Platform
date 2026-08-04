d1e454e047a8f48bb90a493047ff3aaa12e275ef
Shambel Dechu <shambeldechu@gmail.com>
2026-08-04 11:14:39 +0300

    feat: add student-specific endpoints

--- Backend/routers/StudentRouter.js
+++ Backend/routers/StudentRouter.js
@@ -1,9 +1,37 @@
 const express = require("express");
-const { getStudentCourses } = require("../controllers/enrollmentControllers");
-const { protect, authorize } = require("../middlewares/authMiddleware");
+const {
+  getStudentCourses,
+  getCourseOverview,
+  submitAssignment,
+  getMySubmission,
+} = require("../controllers/studentControllers");
+const { protect } = require("../middlewares/authMiddleware");
+const { isStudent, verifyEnrollment } = require("../middlewares/StudentMiddleware");
 
 const router = express.Router();
 
-router.get("/my-courses", protect, authorize("Student"), getStudentCourses);
+router.get("/my-courses", protect, isStudent, getStudentCourses);
+
+router.get(
+  "/courses/:courseId",
+  protect,
+  isStudent,
+  verifyEnrollment,
+  getCourseOverview
+);
+
+router.post(
+  "/assignments/:assignmentId/submit",
+  protect,
+  isStudent,
+  submitAssignment
+);
+
+router.get(
+  "/assignments/:assignmentId/submission",
+  protect,
+  isStudent,
+  getMySubmission
+);
 
 module.exports = router;
